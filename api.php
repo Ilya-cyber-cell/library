717350e7d20ecb9d313aca46b555bcae74ac51f4
Ilya Voronin <voronin.i.a@yandex.ru>
2020-11-29 14:28:50 +0300

    add construct for book class

--- api.php
+++ api.php
@@ -1,8 +1,13 @@
 <?php
 require('./lib/config.php');
 require('./lib/user.php');
+require('./lib/book.php');
 
 $user=new UserClass($dbh,"ivanov",0);
 print ($user->lastName);
 print_r($user->getRights());
+print ("<br>");
+$book=new BookClass($dbh,66541);
+print_r($book);
+
 ?>
